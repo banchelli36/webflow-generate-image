f9969cf7763cd9db5558b8c5e68fb949bd6910a6
banchelli36 <a.banchelli36@gmail.com>
2020-12-21 21:21:10 +0100

    changed image name

--- server.js
+++ server.js
@@ -22,36 +22,36 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/generate", jsonParser, (req, res) => {
+app.get("/generate-all", jsonParser, (req, res) => {
   console.log("webhook triggered: ", req.body);
-  //   try {
-  //     let entries = [];
-  //     requestLocationData(0).then((response) => {
-  //       const locationData = response.data;
-  //       entries = [...response.data.items];
-  //       let promiseRequests = [];
-  //       if (locationData.count < locationData.total) {
-  //         for (
-  //           let i = locationData.count;
-  //           i < locationData.total;
-  //           i = i + locationData.limit
-  //         ) {
-  //           promiseRequests.push(requestLocationData(i));
-  //         }
-  //         Promise.all(promiseRequests).then((result) => {
-  //           result.forEach((item) => {
-  //             entries = [...entries, ...item.data.items];
-  //           });
-  //           generateImages(entries);
-  //           return res.status(200).json({
-  //             data: entries.length,
-  //           });
-  //         });
-  //       }
-  //     });
-  //   } catch (err) {
-  //     console.log("error: ", err);
-  //   }
+  try {
+    let entries = [];
+    requestLocationData(0).then((response) => {
+      const locationData = response.data;
+      entries = [...response.data.items];
+      let promiseRequests = [];
+      if (locationData.count < locationData.total) {
+        for (
+          let i = locationData.count;
+          i < locationData.total;
+          i = i + locationData.limit
+        ) {
+          promiseRequests.push(requestLocationData(i));
+        }
+        Promise.all(promiseRequests).then((result) => {
+          result.forEach((item) => {
+            entries = [...entries, ...item.data.items];
+          });
+          generateImages(entries);
+          return res.status(200).json({
+            data: entries.length,
+          });
+        });
+      }
+    });
+  } catch (err) {
+    console.log("error: ", err);
+  }
   res.status(200).json({
     message: "triggered",
   });
@@ -94,7 +94,7 @@ const generateImages = (locationData) => {
     if (item.latitude && item.longitude) {
       downloadImage(
         `http://maps.googleapis.com/maps/api/staticmap?size=500x456&center=${item.latitude},${item.longitude}&zoom=16&style=visibility:on&style=feature:water%7Celement:geometry%7Cvisibility:on&style=feature:landscape%7Celement:geometry%7Cvisibility:on&style=feature:landscape%7Celement:all%7Ccolor:0xf2f2f2&style=feature:poi|visibility:off&style=feature:administrative%7Celement:labels.text.fill%7Ccolor:0x444444&style=feature:road.highway%7Celement:all%7Cvisibility:simplified&style=feature:road%7Celement:all%7Csaturation:-100&style=feature:road%7Celement:all%7Clightness:45&style=feature:road.arterial%7Celement:labels.icon%7Cvisibility:off&style=feature:water%7Celement:geometry%7Ccolor:0xc0e4f3&markers=icon:https://uploads-ssl.webflow.com/5ea4822fd3a80f6c9cc4fdd9/5f87fae52f748c7c5ad55614_5f81e4e7374a417200dc2551_Geo_Tag.png%7Clabel:S%7C${item.latitude},${item.longitude}&key=AIzaSyA3foPM-dJbV6EXfYoC-zb7-ZY8vcjyiNo`,
-        `public/studio-images/${item.name}(${item._id}).png`,
+        `public/studio-images/hotworx-map-${item.slug}.png`,
         () => {}
       );
     }
